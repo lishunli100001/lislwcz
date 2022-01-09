dcac2f7d9cc08774267f986afe7dbf299568ef95
shunli100001 <shunli_100001@163.com>
2022-01-09 11:45:20 +0800

    feature lisl

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 李顺利
+ * @Date: 2022-01-08 11:54:56
+ * @LastEditors: Andy
+ * @LastEditTime: 2022-01-09 11:44:58
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page lisl</h1>
   </div>
 </template>
